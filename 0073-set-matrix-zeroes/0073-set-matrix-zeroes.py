d47d346cb304de48f9b6d1b9cd013b02dba90134
atharvaawaghad-art <atharvaawaghad@gmail.com>
2026-07-22 20:46:31 +0530

    Time: 6 ms (90.95%), Space: 13.7 MB (14.91%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,23 +1,17 @@
 class Solution(object):
-    def infinity(self,matrix,row,col):
-        r=len(matrix)
-        c=len(matrix[0])
-        for i in range(0,r):
-            if matrix[i][col]!=0:
-                matrix[i][col]=float("inf")
-        for j in range(0,c):
-            if matrix[row][j]!=0:
-                matrix[row][j]=float("inf")
     def setZeroes(self, matrix):
         r=len(matrix)
         c=len(matrix[0])
+        row_track=[0 for _ in range(r)]
+        col_track=[0 for _ in range(c)]
         for i in range(0,r):
             for j in range(0,c):
                 if matrix[i][j]==0:
-                    self.infinity(matrix,i,j)
+                    row_track[i]=-1
+                    col_track[j]=-1
         for i in range(0,r):
             for j in range(0,c):
-                if matrix[i][j]==float("inf"):
+                if row_track[i]==-1 or col_track[j]==-1:
                     matrix[i][j]=0
         
                     
