2cc04e4c6ec5430a2051f14ac6c7dd9add9114e3
atharvaawaghad-art <atharvaawaghad@gmail.com>
2026-07-30 21:38:01 +0530

    Time: 0 ms (100%), Space: 12.4 MB (22.59%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -1,13 +1,8 @@
 class Solution(object):
     def middleNode(self, head):
-        count=0
-        temp=head
-        while temp:
-            count+=1
-            temp=temp.next
-        middle=count//2
-        temp=head
-        while middle>0:
-            temp=temp.next
-            middle-=1
-        return temp
+        slow=head
+        fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
