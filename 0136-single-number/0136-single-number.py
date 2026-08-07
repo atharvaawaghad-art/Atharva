656e7767e75259754abc0faa6454ea4652ec7850
atharvaawaghad-art <atharvaawaghad@gmail.com>
2026-08-07 23:21:53 +0530

    Time: 0 ms (100%), Space: 13.9 MB (79.52%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,14 +1,9 @@
 class Solution(object):
     def singleNumber(self, nums):
-        dic={}
-        for i in nums:
-            if i in dic:
-                dic[i]+=1
-            else:
-                dic[i]=1
-        for key in dic:
-            if dic[key]==1:
-                return key   
+        ans=0
+        for num in nums:
+            ans=ans^num
+        return ans 
                 
 
         
