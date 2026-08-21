2be38255a8d7185603db421d1440344eb6095b8e
atharvaawaghad-art <atharvaawaghad@gmail.com>
2026-08-21 12:07:43 +0530

    Time: 0 ms (100%), Space: 87.1 MB (96.6%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n=bills.size();
+        int five=0;
+        int ten=0;
+        for(int i=0; i < n; i++){
+            if(bills[i] == 5){
+                five++;
+            }
+            else if(bills[i] == 10){
+                if(five >= 1){
+                    ten++;
+                    five--;
+                }
+                else{
+                    return false;
+                }
+            }
+             else{
+                if(five >=1 && ten>=1){
+                    ten--;
+                    five--;
+                }
+                else if(five>=3){
+                    five=five-3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
