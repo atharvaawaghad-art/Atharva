181ec5ca7e38efdcb3bba917cc200ef2a12b2bd2
atharvaawaghad-art <atharvaawaghad@gmail.com>
2026-09-16 13:33:37 +0530

    Time: 0 ms (100%), Space: 64.8 MB (92.14%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    void solve(TreeNode* root,int low,int high,int &ans){
-        if (root==nullptr){
-            return;
-        }
-        if (low<=root->val && root->val<=high){
-            ans+=root->val;
-        }
-        solve(root->left,low,high,ans);
-        solve(root->right,low,high,ans);
-    }
     int rangeSumBST(TreeNode* root, int low, int high) {
-        int ans=0;
-        solve(root,low,high,ans);
-        return ans;
+       if (root==nullptr){
+        return 0;
+       } 
+       if (root->val<low){
+        return rangeSumBST(root->right,low,high);
+       }
+       if (root->val>high){
+        return rangeSumBST(root->left,low,high);
+       }
+       return root->val+rangeSumBST(root->left,low,high)+rangeSumBST(root->right,low,high);
     }
 };
