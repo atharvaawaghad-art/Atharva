be2a9c5592cef8f5737e0162e099028dab536204
atharvaawaghad-art <atharvaawaghad@gmail.com>
2026-09-19 13:19:33 +0530

    Time: 17 ms (10.31%), Space: 8.5 MB (50.84%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x;
+        for (int i = 1; i <= x / 2; i++) {
+            if ((long long)i * i > x) {
+                return i - 1;
+            }
+        }
+        return x / 2;
+    }
+};
